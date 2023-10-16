fb559a20ad36eba714a9634d8c6eb8ac4368d39c
Юрий Селезнев <seleznev_y@mail.ru>
2023-10-16 19:46:10 +0500

    Домашнее задание к семинару №1

---
+++ Task#2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число1: ");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int b = Int32.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(b);
+}
---
+++ Task#4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите число1: ");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int b = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число3: ");
+int c = Int32.Parse(Console.ReadLine());
+int max = a;
+
+if (b > a)
+    {
+        max = b;
+    }
+if (c > max)
+{
+    max = c;
+}
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
